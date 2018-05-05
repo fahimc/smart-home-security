e4023cdec21584e458550d292e11399955352a3b
fahim chowdhury <fahim.chowdhury1985@gmail.com>
2018-05-05 01:06:45 +0100

    added express

--- index.js
+++ index.js
@@ -59,3 +59,10 @@ rfEmitter.sendCode(1234, {pin: 0})
   }, function(error) {
     console.log('Code was not sent, reason: ', error);
   });
+
+var express = require('express')
+var app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
